a7dee8210328349f6ff60e75ce4d73b730d9530f
kamenshik <howstonk@gmail.com>
2023-01-16 00:06:28 +0300

    first

---
+++ src/main/java/lab/fourth/lab/LabApplication.java
@@ -0,0 +1,21 @@
+package lab.fourth.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
