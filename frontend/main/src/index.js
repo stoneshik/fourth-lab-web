d26268f6e004bd35cf6e9d214f86c2875157e506
kamenshik <howstonk@gmail.com>
2023-02-10 22:57:05 +0300

    снова разделил фронтенд на 2 разных проекта, собрал их и залил на бэк

--- frontend/main/src/index.js
+++ frontend/main/src/index.js
@@ -1,30 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import StartPage from "./pages/StartPage";
 import MainPage from "./pages/MainPage";
 import store from "./redux/store";
 
 
-const router = createBrowserRouter(
-    [
-        {
-            path: "/",
-            element: <StartPage />
-        },
-        {
-            path: "/main",
-            element: <MainPage />
-        }
-    ]
-);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <Provider store={store}>
-          <RouterProvider router={router}/>
+          <MainPage/>
       </Provider>
   </React.StrictMode>
 );
