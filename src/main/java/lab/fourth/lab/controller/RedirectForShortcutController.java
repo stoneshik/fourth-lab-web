61f2def4582f40feb139386418549db3f6e9fbb9
kamenshik <howstonk@gmail.com>
2023-02-14 18:51:33 +0300

    набросал структуру + редиректы для сокращенных путей

---
+++ src/main/java/lab/fourth/lab/controller/RedirectForShortcutController.java
@@ -0,0 +1,17 @@
+package lab.fourth.lab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class RedirectForShortcutController {
+    @GetMapping("/main")
+    public RedirectView redirectForMainPage() {
+        return new RedirectView("/main.html");
+    }
+    @GetMapping("/index")
+    public RedirectView redirectForIndexPage() {
+        return new RedirectView("/index.html");
+    }
+}
--- src/main/java/lab/fourth/lab/repository/DotRepository.java
+++ src/main/java/lab/fourth/lab/repository/DotRepository.java
@@ -1,5 +1,6 @@
-package lab.fourth.lab.entities;
+package lab.fourth.lab.repository;
 
+import lab.fourth.lab.entities.Dot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DotRepository extends JpaRepository<Dot, Long> {
