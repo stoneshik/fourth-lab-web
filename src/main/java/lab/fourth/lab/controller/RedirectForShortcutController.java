e9ef586abaca01ea58af5b02ed49c0b41beb9145
kamenshik <howstonk@gmail.com>
2023-02-17 22:26:34 +0300

    настроил доступ к страницам

--- src/main/java/lab/fourth/lab/controller/RedirectForShortcutController.java
+++ src/main/java/lab/fourth/lab/controller/RedirectForShortcutController.java
@@ -2,17 +2,13 @@ package lab.fourth.lab.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class RedirectForShortcutController {
     @GetMapping("/main")
-    public String redirectForMainPage() {
-        return "forward:/main.html";
+    public RedirectView redirectForMainPage() {
+        return new RedirectView("/main.html");
     }
 
     @GetMapping("/index")
--- src/main/java/lab/fourth/lab/setting/SpringSecurityConfig.java
+++ src/main/java/lab/fourth/lab/setting/SpringSecurityConfig.java
@@ -1,6 +1,5 @@
 package lab.fourth.lab.setting;
 
-import lab.fourth.lab.security.JwtCsrfFilter;
 import lab.fourth.lab.security.JwtTokenRepository;
 import lab.fourth.lab.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Configuration
@@ -20,35 +17,46 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService service;
 
-    private final JwtTokenRepository jwtTokenRepository;
-
     @Qualifier("handlerExceptionResolver")
     private final HandlerExceptionResolver resolver;
 
     public SpringSecurityConfig(
             @Autowired UserService userService,
-            @Autowired JwtTokenRepository jwtTokenRepository,
             @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
         this.service = userService;
-        this.jwtTokenRepository = jwtTokenRepository;
         this.resolver = resolver;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.NEVER)
-            .and()
-            .addFilterAt(new JwtCsrfFilter(jwtTokenRepository, resolver), CsrfFilter.class)
-            .csrf().ignoringAntMatchers("/**")
-            .and()
+            .csrf()
+                .disable()
             .authorizeRequests()
-            .antMatchers("/auth/login")
-            .authenticated()
+                //Доступ только для не зарегистрированных пользователей
+                .antMatchers("/api/user/registration", "/api/user/auth").not().fullyAuthenticated()
+                //Доступ только для пользователей с ролью Администратор
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/news").hasRole("USER")
+                //Доступ разрешен всем пользователей
+                .antMatchers("/", "/index", "/index.html", "/static/**").permitAll()
+                //Все остальные страницы требуют аутентификации
+            .anyRequest().authenticated()
             .and()
-            .httpBasic()
-            .authenticationEntryPoint(((request, response, e) -> resolver.resolveException(request, response, null, e)));
+                //Настройка для входа в систему
+                .formLogin()
+                .loginPage("/")
+                //Перенаправление на главную страницу после успешного входа
+                .defaultSuccessUrl("/main")
+                .permitAll()
+            .and()
+                .logout()
+                .permitAll()
+                .logoutSuccessUrl("/")
+            .and()
+                .httpBasic()
+                .authenticationEntryPoint(
+                        ((request, response, e) -> resolver.resolveException(request, response, null, e)));
     }
 
     @Override
